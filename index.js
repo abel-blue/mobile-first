d992755cf3fd862509ca74b591b8637ed6345aa1
Abel Mitiku <abelmgetnet@gmail.com>
2023-03-07 06:04:47 +0300

    add javascript dom manipulation for the menu button

---
+++ index.js
@@ -0,0 +1,31 @@
+const linkPortfolio = document.querySelector(".port-link");
+const linkAbout = document.querySelector(".abt-link");
+const linkContact = document.querySelector(".contact-link");
+const menuBtn = document.querySelector(".menu-button");
+const btnCloseNav = document.querySelector(".btn-close-nav");
+const headerNavMobile = document.querySelector(".mobile-navigation");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.add("hide");
+  headerNavMobile.classList.remove("hide");
+});
+
+btnCloseNav.addEventListener("click", () => {
+  menuBtn.classList.remove("hide");
+  headerNavMobile.classList.add("hide");
+});
+
+linkPortfolio.addEventListener("click", () => {
+  headerNavMobile.classList.add("hide");
+  menuBtn.classList.remove("hide");
+});
+
+linkAbout.addEventListener("click", () => {
+  headerNavMobile.classList.add("hide");
+  menuBtn.classList.remove("hide");
+});
+
+linkContact.addEventListener("click", () => {
+  headerNavMobile.classList.add("hide");
+  menuBtn.classList.remove("hide");
+});
